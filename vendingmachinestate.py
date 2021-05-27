52ec06c4f4b6762b29ab7c310f6efdafd0c267ac
Richard Kasperowski <r@kasperowski.com>
2021-05-27 17:45:21 -0400

    Move VendingMachineState and its subclasses to its own file

---
+++ vendingmachinestate.py
@@ -0,0 +1,47 @@
+from abc import abstractmethod
+
+
+class VendingMachineState:
+    @staticmethod
+    def _display_amount(amount: int) -> str:
+        return '${:,.2f}'.format(amount / 100)
+
+    @abstractmethod
+    def view_display_message(self, vm):
+        pass
+
+
+class InsertCoinState(VendingMachineState):
+    def view_display_message(self, vm):
+        return "INSERT COIN"
+
+
+class HasCustomerCoinsState(VendingMachineState):
+    def view_display_message(self, vm):
+        return VendingMachineState._display_amount(vm.get_balance())
+
+
+class ThankYouState(VendingMachineState):
+    def view_display_message(self, vm):
+        vm.set_vm_state_to_insert_coin_state()
+        return "THANK YOU"
+
+
+class PriceState(VendingMachineState):
+    def view_display_message(self, vm):
+        vm.set_vm_state(InsertCoinState())
+        return 'PRICE ' + VendingMachineState._display_amount(vm.get_display_price())
+
+
+class SoldOutState(VendingMachineState):
+    def view_display_message(self, vm):
+        if vm.get_balance() == 0:
+            vm.set_vm_state(InsertCoinState())
+        else:
+            vm.set_vm_state(HasCustomerCoinsState())
+        return "SOLD OUT"
+
+
+class ExactChangeOnlyState(VendingMachineState):
+    def view_display_message(self, vm):
+        return "EXACT CHANGE ONLY"
